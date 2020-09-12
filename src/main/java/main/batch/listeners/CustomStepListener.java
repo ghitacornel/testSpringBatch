7b50c93a985eeb87b5599a935951999c6794f804
cornel <ghita.cornel@gmail.com>
2020-09-13 00:31:04 +0300

    logging added

--- src/main/java/main/batch/listeners/CustomStepListener.java
+++ src/main/java/main/batch/listeners/CustomStepListener.java
@@ -1,19 +1,23 @@
 package main.batch.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
 
 public class CustomStepListener implements StepExecutionListener {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomStepListener.class);
+
     @Override
     public void beforeStep(StepExecution stepExecution) {
-        System.err.println(stepExecution);
+        logger.info(String.valueOf(stepExecution));
     }
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        System.err.println(stepExecution);
+        logger.info(String.valueOf(stepExecution));
         return stepExecution.getExitStatus();
     }
 
