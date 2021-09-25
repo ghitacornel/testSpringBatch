6b554c2d52ab2c2616bffe85095c2ac31ff20e66
cornel <ghita.cornel@gmail.com>
2021-09-25 17:22:19 +0300

    better tests

--- src/main/java/main/jobs/csv/JobCsvReadWrite.java
+++ src/main/java/main/jobs/csv/JobCsvReadWrite.java
@@ -16,7 +16,6 @@ import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
