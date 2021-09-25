1d8a7a3d6fd838a01f3c23c5ebbf8b6e5aedfc78
cornel <ghita.cornel@gmail.com>
2021-09-25 17:23:00 +0300

    better tests

--- src/test/java/main/jobs/TestsConfiguration.java
+++ src/test/java/main/jobs/TestsConfiguration.java
@@ -2,13 +2,10 @@ package main.jobs;
 
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 @SpringBatchTest
@@ -16,15 +13,6 @@ public abstract class TestsConfiguration {
 
     @Autowired
     protected JobLauncherTestUtils jobLauncherTestUtils;
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Autowired
-    private JobRepository jobRepository;
-
-    @Autowired
-    private JobLauncher jobLauncher;
 
     protected JobParameters defaultJobParameters() {
         JobParametersBuilder builder = new JobParametersBuilder();
