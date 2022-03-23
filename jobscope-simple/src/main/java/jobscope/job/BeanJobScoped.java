40f0adcd7b388e78b483134fb8044105f3c78520
cornel <ghita.cornel@gmail.com>
2022-03-23 14:34:13 +0200

    better

--- jobscope-simple/src/main/java/jobscope/job/BeanJobScoped.java
+++ jobscope-simple/src/main/java/jobscope/job/BeanJobScoped.java
@@ -1,16 +1,11 @@
 package jobscope.job;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @JobScope
-@RequiredArgsConstructor
 public class BeanJobScoped {
-
-    private final JobExecution jobExecution;
 
     boolean startJob;
     boolean startStep1;
