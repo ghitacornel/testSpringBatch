7668b377bdfa46a44e5ebcd6571b645043ac740c
ghita <ghita.cornel@gmail.com>
2023-04-14 18:43:59 +0300

    better

--- jobscope-simple/src/main/java/jobscope/job/JobListener.java
+++ jobscope-simple/src/main/java/jobscope/job/JobListener.java
@@ -1,23 +1,25 @@
 package jobscope.job;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @JobScope
 @RequiredArgsConstructor
-public class JobListener implements javax.batch.api.listener.JobListener {
+public class JobListener implements JobExecutionListener {
 
     private final BeanJobScoped beanJobScoped;
 
     @Override
-    public void beforeJob() {
+    public void beforeJob(JobExecution jobExecution) {
         beanJobScoped.startJob = true;
     }
 
     @Override
-    public void afterJob() {
+    public void afterJob(JobExecution jobExecution) {
         beanJobScoped.endJob = true;
 
         // all checks go here
