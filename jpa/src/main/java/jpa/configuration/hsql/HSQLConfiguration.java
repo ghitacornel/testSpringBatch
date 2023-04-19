0b922eff5e3f5763c3755b0471ef022ba4ac5045
ghita <ghita.cornel@gmail.com>
2023-04-19 22:43:21 +0300

    better

--- jpa/src/main/java/jpa/configuration/hsql/HSQLConfiguration.java
+++ jpa/src/main/java/jpa/configuration/hsql/HSQLConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -18,7 +17,6 @@ import java.util.Map;
         transactionManagerRef = "hsqlPTM",
         basePackages = {"jpa.configuration.hsql"}
 )
-@DependsOn({"flywayInitializer"})
 public class HSQLConfiguration {
 
     @Bean
