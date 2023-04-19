fd6a7b048242b68978996ebf29b724d766fb7d4c
ghita <ghita.cornel@gmail.com>
2023-04-19 22:44:50 +0300

    better

--- jpa/src/main/java/jpa/configuration/h2/H2Configuration.java
+++ jpa/src/main/java/jpa/configuration/h2/H2Configuration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
@@ -17,6 +18,7 @@ import java.util.Map;
         transactionManagerRef = "h2PTM",
         basePackages = {"jpa.configuration.h2"}
 )
+@DependsOn({"flywayInitializer"})
 public class H2Configuration {
 
     @Bean
