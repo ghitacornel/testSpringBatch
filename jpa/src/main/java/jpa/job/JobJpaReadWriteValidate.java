20aff2a988a871a0325918820e92f3a09dc93c56
ghita <ghita.cornel@gmail.com>
2023-04-20 09:59:43 +0300

    better

--- jpa/src/main/java/jpa/job/JobJpaReadWriteValidate.java
+++ jpa/src/main/java/jpa/job/JobJpaReadWriteValidate.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.persistence.EntityManagerFactory;
@@ -156,6 +157,10 @@ public class JobJpaReadWriteValidate {
 
                 // writer/LOAD
                 .writer(writer)
+
+                // executor for parallel running
+                .taskExecutor(new SimpleAsyncTaskExecutor("performanceTaskExecutor"))
+                .throttleLimit(5)
 
                 //job configuration done
                 .build();
