1c186b6afab2fdd2dac3d20dc18e0def36b2573e
ghita <ghita.cornel@gmail.com>
2024-02-22 12:03:19 +0200

    better

--- jpa-2-databases/src/main/java/jpa/job/JobSingleThreadConfiguration.java
+++ jpa-2-databases/src/main/java/jpa/job/JobSingleThreadConfiguration.java
@@ -7,7 +7,6 @@ import jpa.configuration.hsql.entity.OutputEntity;
 import jpa.configuration.hsql.repository.OutputEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -39,50 +38,7 @@ class JobSingleThreadConfiguration {
 
     @Bean
     Job jobSingleThread() {
-        return new JobBuilder("jobSingleThread", jobRepository)
-                .incrementer(new RunIdIncrementer())
-                .start(createDataStep())
-                .next(processingStep())
-                .next(verifyDatabaseStep())
-                .build();
-    }
-
-    private Step createDataStep() {
-        return new StepBuilder("createDataStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-
-                    //cleanup INPUT database
-                    inputEntityRepository.deleteAll();
-
-                    //cleanup OUTPUT database
-                    outputEntityRepository.deleteAll();
-
-                    // generate data
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    List<InputEntity> list = InputGenerator.generate(count);
-
-                    // write generated data
-                    inputEntityRepository.saveAll(list);
-
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step verifyDatabaseStep() {// only a count is performed as validation
-        return new StepBuilder("verifyDatabaseStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    long actualCount = outputEntityRepository.count();
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    if (actualCount != count) {
-                        throw new RuntimeException("expected " + count + " found " + actualCount);
-                    }
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step processingStep() {
+        // only a count is performed as validation
 
         JpaPagingItemReader<InputEntity> reader = new JpaPagingItemReader<>();
         reader.setQueryString("select t from InputEntity t");
@@ -94,30 +50,60 @@ class JobSingleThreadConfiguration {
         writer.setEntityManagerFactory(hsqlEMFB);
         writer.setUsePersist(true);
 
-        return new StepBuilder("processingStep", jobRepository)
+        return new JobBuilder("jobSingleThread", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(new StepBuilder("createDummyData", jobRepository)
+                        .tasklet((contribution, chunkContext) -> {
 
-                // larger is faster but requires more memory
-                .<InputEntity, OutputEntity>chunk(1000, transactionManager)
+                            //cleanup databases
+                            inputEntityRepository.deleteAll();
+                            outputEntityRepository.deleteAll();
 
-                // reader/EXTRACT
-                .reader(reader)
+                            // generate data
+                            long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
+                            List<InputEntity> list = InputGenerator.generate(count);
 
-                // processor/TRANSFORM
-                .processor(input -> {
-                    OutputEntity output = new OutputEntity();
-                    output.setId(input.getId());
-                    output.setFirstName(input.getFirstName());
-                    output.setLastName(input.getLastName());
-                    output.setAge(input.getAge() + 1);
-                    output.setSalary(input.getSalary() + 2);
-                    output.setDifference(output.getSalary() - output.getAge());
-                    return output;
-                })
+                            // write generated data
+                            inputEntityRepository.saveAll(list);
 
-                // writer/LOAD
-                .writer(writer)
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
+                        .build())
+                .next(new StepBuilder("migrateData", jobRepository)
 
-                //job configuration done
+                        // larger is faster but requires more memory
+                        .<InputEntity, OutputEntity>chunk(1000, transactionManager)
+
+                        // reader/EXTRACT
+                        .reader(reader)
+
+                        // processor/TRANSFORM
+                        .processor(input -> {
+                            OutputEntity output = new OutputEntity();
+                            output.setId(input.getId());
+                            output.setFirstName(input.getFirstName());
+                            output.setLastName(input.getLastName());
+                            output.setAge(input.getAge() + 1);
+                            output.setSalary(input.getSalary() + 2);
+                            output.setDifference(output.getSalary() - output.getAge());
+                            return output;
+                        })
+
+                        // writer/LOAD
+                        .writer(writer)
+
+                        //job configuration done
+                        .build())
+                .next(new StepBuilder("verifyMigratedData", jobRepository)
+                        .tasklet((contribution1, chunkContext1) -> {
+                            long actualCount = outputEntityRepository.count();
+                            long count1 = (long) chunkContext1.getStepContext().getJobParameters().get("count");
+                            if (actualCount != count1) {
+                                throw new RuntimeException("expected " + count1 + " found " + actualCount);
+                            }
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
+                        .build())
                 .build();
     }
 
