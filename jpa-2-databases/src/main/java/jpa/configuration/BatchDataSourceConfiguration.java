30cb0c9171f1717776fc4701154bb314a2a21b7d
ghita <ghita.cornel@gmail.com>
2024-02-26 16:52:41 +0200

    better

---
+++ jpa-2-databases/src/main/java/jpa/configuration/BatchDataSourceConfiguration.java
@@ -0,0 +1,40 @@
+package jpa.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+class BatchDataSourceConfiguration {
+
+    @Value("${spring.datasource.batch.url}")
+    private String url;
+
+    @Value("${spring.datasource.batch.username}")
+    private String username;
+
+    @Value("${spring.datasource.batch.password}")
+    private String password;
+
+    @Value("${spring.datasource.batch.driver}")
+    private String driver;
+
+    // the default data source to be used by Spring Batch
+    // Spring batch always needs at least 1 data source
+    @Primary
+    @Bean
+    DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        return new HikariDataSource(config);
+    }
+
+}
