c3f086e06af6652515a428fba1f45217b8627412
ghita <ghita.cornel@gmail.com>
2024-02-26 16:04:04 +0200

    better

--- jpa-2-databases/src/main/java/jpa/controllers/JobsController.java
+++ jpa-2-databases/src/main/java/jpa/controllers/JobsController.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,11 +31,11 @@ public class JobsController {
     private final JobLauncher jobLauncher;
 
     @GetMapping("jobJpaReadWriteErrorHandling")
-    public void jobJpaReadWriteErrorHandling() {
+    public void jobJpaReadWriteErrorHandling(@RequestParam(value = "count", defaultValue = "10000") long count) {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis())
-                    .addLong("count", 10000L)
+                    .addLong("count", count)
                     .toJobParameters();
             jobLauncher.run(jobJpaReadWriteErrorHandling, jobParameters);
         } catch (Exception e) {
@@ -43,11 +44,11 @@ public class JobsController {
     }
 
     @GetMapping("jobJpaReadWritePerformanceMultipleThreads")
-    public void jobJpaReadWritePerformanceMultipleThreads() {
+    public void jobJpaReadWritePerformanceMultipleThreads(@RequestParam(value = "count", defaultValue = "10000") long count) {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis())
-                    .addLong("count", 10000L)
+                    .addLong("count", count)
                     .toJobParameters();
             jobLauncher.run(jobJpaReadWritePerformanceMultipleThreads, jobParameters);
         } catch (Exception e) {
@@ -56,11 +57,11 @@ public class JobsController {
     }
 
     @GetMapping("jobJpaReadWriteValidate")
-    public void jobJpaReadWriteValidate() {
+    public void jobJpaReadWriteValidate(@RequestParam(value = "count", defaultValue = "10000") long count) {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis())
-                    .addLong("count", 10000L)
+                    .addLong("count", count)
                     .toJobParameters();
             jobLauncher.run(jobJpaReadWriteValidate, jobParameters);
         } catch (Exception e) {
@@ -69,11 +70,11 @@ public class JobsController {
     }
 
     @GetMapping("jobSingleThread")
-    public void jobSingleThread() {
+    public void jobSingleThread(@RequestParam(value = "count", defaultValue = "10000") long count) {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis())
-                    .addLong("count", 10000L)
+                    .addLong("count", count)
                     .toJobParameters();
             jobLauncher.run(jobSingleThread, jobParameters);
         } catch (Exception e) {
