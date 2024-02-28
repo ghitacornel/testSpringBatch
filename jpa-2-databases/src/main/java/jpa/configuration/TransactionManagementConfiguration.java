b117446a13ec6a10cc7a58fff22c3ea69534a8a0
ghita <ghita.cornel@gmail.com>
2024-02-28 20:02:44 +0200

    better

--- jpa-2-databases/src/main/java/jpa/configuration/TransactionManagementConfiguration.java
+++ jpa-2-databases/src/main/java/jpa/configuration/TransactionManagementConfiguration.java
@@ -4,17 +4,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.transaction.ChainedTransactionManager;
-import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 class TransactionManagementConfiguration {
 
     @Primary
     @Bean
-    PlatformTransactionManager transactionManager(@Qualifier("inputTransactionManager") JpaTransactionManager jpaTransactionManager1, @Qualifier("outputTransactionManager") JpaTransactionManager jpaTransactionManager2) {
-        return new ChainedTransactionManager(jpaTransactionManager1, jpaTransactionManager2);
+    PlatformTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
     }
 
 }
