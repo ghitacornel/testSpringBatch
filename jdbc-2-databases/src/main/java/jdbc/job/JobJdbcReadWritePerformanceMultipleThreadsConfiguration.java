654ae9285a9e26c33ff49d9bbf9f8007e16fca4d
ghita <ghita.cornel@gmail.com>
2024-02-29 22:57:51 +0200

    better

--- jdbc-2-databases/src/main/java/jdbc/job/JobJdbcReadWritePerformanceMultipleThreadsConfiguration.java
+++ jdbc-2-databases/src/main/java/jdbc/job/JobJdbcReadWritePerformanceMultipleThreadsConfiguration.java
@@ -3,7 +3,6 @@ package jdbc.job;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -45,118 +44,7 @@ class JobJdbcReadWritePerformanceMultipleThreadsConfiguration {
 
     @Bean
     Job jobJdbcReadWritePerformanceMultipleThreads() {
-        return new JobBuilder("jobJdbcReadWritePerformanceMultipleThreads", jobRepository)
-                .incrementer(new RunIdIncrementer())
-                .start(createDataStep())
-                .next(processingStep())
-                .next(verifyDatabaseStep())
-                .build();
-    }
-
-    private Step createDataStep() {
-        return new StepBuilder("createDataStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-
-                    //cleanup INPUT database
-                    {
-                        Connection connection = dataSourceH2.getConnection();
-                        Statement statement = connection.createStatement();
-                        statement.executeUpdate("truncate table InputDTO");
-                        statement.close();
-                    }
-
-                    //cleanup OUTPUT database
-                    {
-                        Connection connection = dataSourceHSQL.getConnection();
-                        Statement statement = connection.createStatement();
-                        statement.executeUpdate("truncate table OutputDTO");
-                        statement.close();
-                    }
-
-                    // generate data
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    List<InputDTO> list = InputGenerator.generate(count);
-
-                    // write generated data
-                    Connection connection = dataSourceH2.getConnection();
-                    PreparedStatement preparedStatement = connection.prepareStatement("insert into InputDTO(ID,firstName,lastName,salary,age) values(?,?,?,?,?)");
-                    for (InputDTO inputDTO : list) {
-                        preparedStatement.setInt(1, inputDTO.getId());
-                        preparedStatement.setString(2, inputDTO.getFirstName());
-                        preparedStatement.setString(3, inputDTO.getLastName());
-                        preparedStatement.setLong(4, inputDTO.getSalary());
-                        preparedStatement.setInt(5, inputDTO.getAge());
-                        preparedStatement.execute();
-                    }
-                    preparedStatement.close();
-                    connection.close();
-
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step verifyDatabaseStep() {// only a count is performed as validation
-        return new StepBuilder("verifyDatabaseStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    Connection connection = dataSourceHSQL.getConnection();
-                    PreparedStatement preparedStatement = connection.prepareStatement("select count(*) from OutputDTO");
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    resultSet.next();
-                    long actualCount = resultSet.getLong(1);
-                    preparedStatement.close();
-                    connection.close();
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    if (actualCount != count) {
-                        throw new RuntimeException("expected " + count + " found " + actualCount);
-                    }
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step processingStep() {
-        return new StepBuilder("processingStep", jobRepository)
-
-                // larger is faster but requires more memory
-                .<InputDTO, OutputDTO>chunk(1000, transactionManager)
-
-                .reader(reader())
-
-                // processor/TRANSFORM
-                .processor(input -> {
-                    OutputDTO output = new OutputDTO();
-                    output.setId(input.getId());
-                    output.setFirstName(input.getFirstName());
-                    output.setLastName(input.getLastName());
-                    output.setAge(input.getAge() + 1);
-                    output.setSalary(input.getSalary() + 2);
-                    output.setDifference(output.getSalary() - output.getAge());
-                    return output;
-                })
-
-                // writer/LOAD
-                .writer(new JdbcBatchItemWriterBuilder<OutputDTO>()
-                        .dataSource(dataSourceHSQL)
-                        .sql("INSERT INTO OutputDTO(id,firstName,lastName,salary,age,difference) VALUES (?,?,?,?,?,?)")
-                        .itemPreparedStatementSetter((item, ps) -> {
-                            ps.setInt(1, item.getId());
-                            ps.setString(2, item.getFirstName());
-                            ps.setString(3, item.getLastName());
-                            ps.setLong(4, item.getSalary());
-                            ps.setInt(5, item.getAge());
-                            ps.setLong(6, item.getDifference());
-                        })
-                        .build())
-
-                // executor for parallel running
-                .taskExecutor(new SimpleAsyncTaskExecutor("performanceTaskExecutor"))
-
-                //job configuration done
-                .build();
-    }
-
-    private JdbcPagingItemReader<InputDTO> reader() {
+        // only a count is performed as validation
 
         H2PagingQueryProvider queryProvider = new H2PagingQueryProvider();
         queryProvider.setSelectClause("*");
@@ -182,7 +70,103 @@ class JobJdbcReadWritePerformanceMultipleThreadsConfiguration {
             log.error("reader error", e);
         }
 
-        return jdbcPagingItemReader;
+        return new JobBuilder("jobJdbcReadWritePerformanceMultipleThreads", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(new StepBuilder("createDataStep", jobRepository)
+                        .tasklet((contribution, chunkContext) -> {
+
+                            //cleanup INPUT database
+                            {
+                                Connection connection = dataSourceH2.getConnection();
+                                Statement statement = connection.createStatement();
+                                statement.executeUpdate("truncate table InputDTO");
+                                statement.close();
+                            }
+
+                            //cleanup OUTPUT database
+                            {
+                                Connection connection = dataSourceHSQL.getConnection();
+                                Statement statement = connection.createStatement();
+                                statement.executeUpdate("truncate table OutputDTO");
+                                statement.close();
+                            }
+
+                            // generate data
+                            long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
+                            List<InputDTO> list = InputGenerator.generate(count);
+
+                            // write generated data
+                            Connection connection = dataSourceH2.getConnection();
+                            PreparedStatement preparedStatement = connection.prepareStatement("insert into InputDTO(ID,firstName,lastName,salary,age) values(?,?,?,?,?)");
+                            for (InputDTO inputDTO : list) {
+                                preparedStatement.setInt(1, inputDTO.getId());
+                                preparedStatement.setString(2, inputDTO.getFirstName());
+                                preparedStatement.setString(3, inputDTO.getLastName());
+                                preparedStatement.setLong(4, inputDTO.getSalary());
+                                preparedStatement.setInt(5, inputDTO.getAge());
+                                preparedStatement.execute();
+                            }
+                            preparedStatement.close();
+                            connection.close();
+
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
+                        .build())
+                .next(new StepBuilder("processingStep", jobRepository)
+
+                        // larger is faster but requires more memory
+                        .<InputDTO, OutputDTO>chunk(1000, transactionManager)
+
+                        .reader(jdbcPagingItemReader)
+
+                        // processor/TRANSFORM
+                        .processor(input -> {
+                            OutputDTO output = new OutputDTO();
+                            output.setId(input.getId());
+                            output.setFirstName(input.getFirstName());
+                            output.setLastName(input.getLastName());
+                            output.setAge(input.getAge() + 1);
+                            output.setSalary(input.getSalary() + 2);
+                            output.setDifference(output.getSalary() - output.getAge());
+                            return output;
+                        })
+
+                        // writer/LOAD
+                        .writer(new JdbcBatchItemWriterBuilder<OutputDTO>()
+                                .dataSource(dataSourceHSQL)
+                                .sql("INSERT INTO OutputDTO(id,firstName,lastName,salary,age,difference) VALUES (?,?,?,?,?,?)")
+                                .itemPreparedStatementSetter((item, ps) -> {
+                                    ps.setInt(1, item.getId());
+                                    ps.setString(2, item.getFirstName());
+                                    ps.setString(3, item.getLastName());
+                                    ps.setLong(4, item.getSalary());
+                                    ps.setInt(5, item.getAge());
+                                    ps.setLong(6, item.getDifference());
+                                })
+                                .build())
+
+                        // executor for parallel running
+                        .taskExecutor(new SimpleAsyncTaskExecutor("performanceTaskExecutor"))
+
+                        //job configuration done
+                        .build())
+                .next(new StepBuilder("verifyDatabaseStep", jobRepository)
+                        .tasklet((contribution1, chunkContext1) -> {
+                            Connection connection1 = dataSourceHSQL.getConnection();
+                            PreparedStatement preparedStatement1 = connection1.prepareStatement("select count(*) from OutputDTO");
+                            ResultSet resultSet = preparedStatement1.executeQuery();
+                            resultSet.next();
+                            long actualCount = resultSet.getLong(1);
+                            preparedStatement1.close();
+                            connection1.close();
+                            long count1 = (long) chunkContext1.getStepContext().getJobParameters().get("count");
+                            if (actualCount != count1) {
+                                throw new RuntimeException("expected " + count1 + " found " + actualCount);
+                            }
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
+                        .build())
+                .build();
     }
 
 }
--- jdbc-2-databases/src/main/java/jdbc/job/JobJdbcReadWritePerformanceSingleThreadConfiguration.java
+++ jdbc-2-databases/src/main/java/jdbc/job/JobJdbcReadWritePerformanceSingleThreadConfiguration.java
@@ -2,7 +2,6 @@ package jdbc.job;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -38,117 +37,106 @@ class JobJdbcReadWritePerformanceSingleThreadConfiguration {
 
     @Bean
     Job jobJdbcReadWritePerformanceSingleThread() {
+        // only a count is performed as validation
         return new JobBuilder("jobJdbcReadWritePerformanceSingleThread", jobRepository)
                 .incrementer(new RunIdIncrementer())
-                .start(createDataStep())
-                .next(processingStep())
-                .next(verifyDatabaseStep())
-                .build();
-    }
+                .start(new StepBuilder("createDataStep", jobRepository)
+                        .tasklet((contribution, chunkContext) -> {
 
-    private Step createDataStep() {
-        return new StepBuilder("createDataStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
+                            //cleanup INPUT database
+                            {
+                                Connection connection = dataSourceH2.getConnection();
+                                Statement statement = connection.createStatement();
+                                statement.executeUpdate("truncate table InputDTO");
+                                statement.close();
+                            }
 
-                    //cleanup INPUT database
-                    {
-                        Connection connection = dataSourceH2.getConnection();
-                        Statement statement = connection.createStatement();
-                        statement.executeUpdate("truncate table InputDTO");
-                        statement.close();
-                    }
+                            //cleanup OUTPUT database
+                            {
+                                Connection connection = dataSourceHSQL.getConnection();
+                                Statement statement = connection.createStatement();
+                                statement.executeUpdate("truncate table OutputDTO");
+                                statement.close();
+                            }
 
-                    //cleanup OUTPUT database
-                    {
-                        Connection connection = dataSourceHSQL.getConnection();
-                        Statement statement = connection.createStatement();
-                        statement.executeUpdate("truncate table OutputDTO");
-                        statement.close();
-                    }
+                            // generate data
+                            long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
+                            List<InputDTO> list = InputGenerator.generate(count);
 
-                    // generate data
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    List<InputDTO> list = InputGenerator.generate(count);
+                            // write generated data
+                            Connection connection = dataSourceH2.getConnection();
+                            PreparedStatement preparedStatement = connection.prepareStatement("insert into InputDTO(ID,firstName,lastName,salary,age) values(?,?,?,?,?)");
+                            for (InputDTO inputDTO : list) {
+                                preparedStatement.setInt(1, inputDTO.getId());
+                                preparedStatement.setString(2, inputDTO.getFirstName());
+                                preparedStatement.setString(3, inputDTO.getLastName());
+                                preparedStatement.setLong(4, inputDTO.getSalary());
+                                preparedStatement.setInt(5, inputDTO.getAge());
+                                preparedStatement.execute();
+                            }
+                            preparedStatement.close();
+                            connection.close();
 
-                    // write generated data
-                    Connection connection = dataSourceH2.getConnection();
-                    PreparedStatement preparedStatement = connection.prepareStatement("insert into InputDTO(ID,firstName,lastName,salary,age) values(?,?,?,?,?)");
-                    for (InputDTO inputDTO : list) {
-                        preparedStatement.setInt(1, inputDTO.getId());
-                        preparedStatement.setString(2, inputDTO.getFirstName());
-                        preparedStatement.setString(3, inputDTO.getLastName());
-                        preparedStatement.setLong(4, inputDTO.getSalary());
-                        preparedStatement.setInt(5, inputDTO.getAge());
-                        preparedStatement.execute();
-                    }
-                    preparedStatement.close();
-                    connection.close();
-
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step verifyDatabaseStep() {// only a count is performed as validation
-        return new StepBuilder("verifyDatabaseStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    Connection connection = dataSourceHSQL.getConnection();
-                    PreparedStatement preparedStatement = connection.prepareStatement("select count(*) from OutputDTO");
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    resultSet.next();
-                    long actualCount = resultSet.getLong(1);
-                    preparedStatement.close();
-                    connection.close();
-                    long count = (long) chunkContext.getStepContext().getJobParameters().get("count");
-                    if (actualCount != count) {
-                        throw new RuntimeException("expected " + count + " found " + actualCount);
-                    }
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .build();
-    }
-
-    private Step processingStep() {
-        return new StepBuilder("processingStep", jobRepository)
-
-                // larger is faster but requires more memory
-                .<InputDTO, OutputDTO>chunk(1000, transactionManager)
-
-                // reader/EXTRACT
-                .reader(new JdbcCursorItemReaderBuilder<InputDTO>()
-                        .name("jdbcCursorItemReader")
-                        .dataSource(dataSourceH2)
-                        .sql("select * from InputDTO")// programmer is responsible for filtering the data to be processed
-                        .rowMapper(new BeanPropertyRowMapper<>(InputDTO.class))
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
                         .build())
+                .next(new StepBuilder("processingStep", jobRepository)
 
-                // processor/TRANSFORM
-                .processor(input -> {
-                    OutputDTO output = new OutputDTO();
-                    output.setId(input.getId());
-                    output.setFirstName(input.getFirstName());
-                    output.setLastName(input.getLastName());
-                    output.setAge(input.getAge() + 1);
-                    output.setSalary(input.getSalary() + 2);
-                    output.setDifference(output.getSalary() - output.getAge());
-                    return output;
-                })
+                        // larger is faster but requires more memory
+                        .<InputDTO, OutputDTO>chunk(1000, transactionManager)
 
-                // writer/LOAD
-                .writer(new JdbcBatchItemWriterBuilder<OutputDTO>()
-                        .dataSource(dataSourceHSQL)
-                        .sql("INSERT INTO OutputDTO(id,firstName,lastName,salary,age,difference) VALUES (?,?,?,?,?,?)")
-                        .itemPreparedStatementSetter((item, ps) -> {
-                            ps.setInt(1, item.getId());
-                            ps.setString(2, item.getFirstName());
-                            ps.setString(3, item.getLastName());
-                            ps.setLong(4, item.getSalary());
-                            ps.setInt(5, item.getAge());
-                            ps.setLong(6, item.getDifference());
+                        // reader/EXTRACT
+                        .reader(new JdbcCursorItemReaderBuilder<InputDTO>()
+                                .name("jdbcCursorItemReader")
+                                .dataSource(dataSourceH2)
+                                .sql("select * from InputDTO")// programmer is responsible for filtering the data to be processed
+                                .rowMapper(new BeanPropertyRowMapper<>(InputDTO.class))
+                                .build())
+
+                        // processor/TRANSFORM
+                        .processor(input -> {
+                            OutputDTO output = new OutputDTO();
+                            output.setId(input.getId());
+                            output.setFirstName(input.getFirstName());
+                            output.setLastName(input.getLastName());
+                            output.setAge(input.getAge() + 1);
+                            output.setSalary(input.getSalary() + 2);
+                            output.setDifference(output.getSalary() - output.getAge());
+                            return output;
                         })
-                        .build())
 
-                //job configuration done
+                        // writer/LOAD
+                        .writer(new JdbcBatchItemWriterBuilder<OutputDTO>()
+                                .dataSource(dataSourceHSQL)
+                                .sql("INSERT INTO OutputDTO(id,firstName,lastName,salary,age,difference) VALUES (?,?,?,?,?,?)")
+                                .itemPreparedStatementSetter((item, ps) -> {
+                                    ps.setInt(1, item.getId());
+                                    ps.setString(2, item.getFirstName());
+                                    ps.setString(3, item.getLastName());
+                                    ps.setLong(4, item.getSalary());
+                                    ps.setInt(5, item.getAge());
+                                    ps.setLong(6, item.getDifference());
+                                })
+                                .build())
+
+                        //job configuration done
+                        .build())
+                .next(new StepBuilder("verifyDatabaseStep", jobRepository)
+                        .tasklet((contribution1, chunkContext1) -> {
+                            Connection connection1 = dataSourceHSQL.getConnection();
+                            PreparedStatement preparedStatement1 = connection1.prepareStatement("select count(*) from OutputDTO");
+                            ResultSet resultSet = preparedStatement1.executeQuery();
+                            resultSet.next();
+                            long actualCount = resultSet.getLong(1);
+                            preparedStatement1.close();
+                            connection1.close();
+                            long count1 = (long) chunkContext1.getStepContext().getJobParameters().get("count");
+                            if (actualCount != count1) {
+                                throw new RuntimeException("expected " + count1 + " found " + actualCount);
+                            }
+                            return RepeatStatus.FINISHED;
+                        }, transactionManager)
+                        .build())
                 .build();
     }
 
